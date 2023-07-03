d6228a472352f61a482968d478bd4059a4d3117d
Manoj G <manojpgudi@gmail.com>
2023-07-03 22:23:35 +0200

    Adding fixes to relay the sensor data for BME properly | Found a bug on sensor.community APIs

--- webservice.py
+++ webservice.py
@@ -27,20 +27,33 @@ def get_data():
 def relayDataFromLNS():
     """
     The route to recieve the data from TTN and relay it to the sensor.community
+
+    # NOTE
+    Ideally we should be sending 1 request per sensor module, but there's a bug
+    in the sensor.community and hence we have to send all the payload in a single REST API
+    The dashboard DOESNT distinguish SDS & BME as two different sensors
     """
     fromGW = request.get_json(force=True)
     print(fromGW)
     if "data" in fromGW:
         payload = base64.b64decode(fromGW["data"])
 
- 	# Relay the data to sensor.community
+ 	# Relay the SDS data to sensor.community
     headers = {"X-Pin":"1", "X-Sensor" : sensorUID, "Content-Type" : "application/json"}
     requestBody = fromGW["uplink_message"]["decoded_payload"]
     url = "https://api.sensor.community/v1//push-sensor-data/"
     sensorCommunityResponse = requests.post(url, json = requestBody, headers = headers)
 
-    # If you get HTTP 2XX then
-    if int(sensorCommunityResponse.status_code/100) == 2:
+    # TODO When the API bug is resolved Relay the BME280 data to sensor.community
+    """
+    headers = {"X-Pin":"11", "X-Sensor" : sensorUID, "Content-Type" : "application/json"}
+    requestBody = fromGW["uplink_message"]["decoded_payload"]
+    url = "https://api.sensor.community/v1//push-sensor-data/"
+    sensorCommunityResponse2 = requests.post(url, json = requestBody, headers = headers)
+    """
+
+    # If you get HTTP 2XX then move on
+    if (int(sensorCommunityResponse.status_code/100) == 2):
         return make_response("Successfully sent data to sensor-community", 200)
 
     return make_response("Failed :" + sensorCommunityResponse.text, 501)
